897ffd812bc6287d9da57835e3b1f85675533ad1
peyin192 <vuonglinh190206.qv@gmail.com>
2024-11-20 06:51:58 +0700

    upload session 02-2

---
+++ ss02-2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 1;
+    int num2 = 2;
+    int sum = num1 + num2;  
+    printf("tong cua %d va %d la: %d\n", num1, num2, sum);
+
+    return 0;
+}
