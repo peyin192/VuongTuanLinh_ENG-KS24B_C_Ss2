2c63f1b37e768eac4744576198d95e1b28a4fdbd
peyin192 <vuonglinh190206.qv@gmail.com>
2024-11-20 06:38:13 +0700

    upload session 02-1

---
+++ ss02-1.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a = 1;      // khai bao bien a bang 1
+    //luu tru gia tri so nguyen
+     
+    float x = 1.2f; // khai bao bien x bang 1.2
+    //luu tru gia tri so thap phan
+
+    double pi = 3.14159265359; 
+	// khai bao gia tri co do chinh xac cao
+
+    char nam = 'A';   //khai bao bien nam la A
+	// luu tru ki tu don
+
+    unsigned int positiveNum = 25; 
+	// luu tru bien co gia tri khong am
+
+    long long int bigNumber = 1234567890;  //khai bao bien voi gia tri rat lon
+	// luu tru bien co gia tri rat lon
+
+    short int smallNumber = 100; // khai bao bien voi gia tri bang 100
+    //luu tru bien co gia tri nho hon int
+
+    printf("a = %d\n", a);
+    printf("x = %.2f\n", x);
+    //in gia tri cua bien ra man hinh
+
+    return 0;
+}
