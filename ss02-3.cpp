e0e6a52dab49982594984ce2c8f35f862f07c706
peyin192 <vuonglinh190206.qv@gmail.com>
2024-11-20 22:07:27 +0700

    upload session 02-3

--- ss02-3.cpp
+++ ss02-3.cpp
@@ -8,13 +8,17 @@ int main() {
     printf("tong cua %d va %d la: %d\n", num1, num2, sum);
     
     int difference = num1 - num2;
-    printf("hieu cua %d va %d la: %d\n", num1, num2, difference)
+    printf("hieu cua %d va %d la: %d\n", num1, num2, difference);
     
     int product = num1 * num2;
-    printf("tich cua %d va %d la %d\n", num1, num2, product)
-    
-    int quotient = num1 / num2;
-    printf("thuong cua %d va %d la %d\n", num1, num2, quotient )
+    printf("tich cua %d va %d la %d\n", num1, num2, product);
+
+    if (num2 != 0) {
+        result = num1 / num2;
+        printf("thuong cua %d va %d : %d\n", num1, num2, result);
+    } else {
+        printf("loi khong the chia cho 0!\n");
+    }
 
     return 0;
 }
