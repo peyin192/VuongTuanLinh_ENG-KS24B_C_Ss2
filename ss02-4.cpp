73f73caca5cdeef139c76b84ec1bd0d13c84f3fd
peyin192 <vuonglinh190206.qv@gmail.com>
2024-11-20 09:13:58 +0700

    upload ss02-4

---
+++ ss02-4.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float side = 2.0;
+    float perimeter = 4 * side;
+    float area = side * side;
+    
+    printf("canh: %.2f\n", side);
+    printf("chu vi: %.2f\n", perimeter);
+    printf("dien tich: %.2f\n", area);
+
+    return 0;
+}
+
